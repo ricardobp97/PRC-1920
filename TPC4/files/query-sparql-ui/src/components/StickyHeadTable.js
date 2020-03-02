8ba4d2a9754b613e2969efbbc4fa62bd3be0b6b1
Ricardo Pereira <ricardo-97-pereira@hotmail.com>
2020-03-02 14:02:12 +0000

    add table pagination

--- TPC4/files/query-sparql-ui/src/components/StickyHeadTable.js
+++ TPC4/files/query-sparql-ui/src/components/StickyHeadTable.js
@@ -7,6 +7,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import TablePagination from '@material-ui/core/TablePagination';
 
 const useStyles = makeStyles({
   root: {
@@ -19,6 +20,17 @@ const useStyles = makeStyles({
 
 export default function StickyHeadTable(props) {
   const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
   return (
     <Paper className={classes.root}>
@@ -34,7 +46,7 @@ export default function StickyHeadTable(props) {
             </TableRow>
           </TableHead>
           <TableBody>
-            {props.results.map(row => {
+            {props.results.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
               return (
                 <TableRow>
                     {props.vars.map(column => {
@@ -52,6 +64,15 @@ export default function StickyHeadTable(props) {
           </TableBody>
         </Table>
       </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 20]}
+        component="div"
+        count={props.results.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
     </Paper>
   );
 }
